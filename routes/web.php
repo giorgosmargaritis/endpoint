b88df8b11379f108ea95acac68c9c5df7b7e44c0
Giorgos Margaritis <gmargaritis17@gmail.com>
2023-09-13 08:42:44 +0300

    Log in home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    Log::info($request->all());
+    return view('welcome', [], ['200']);
 });
