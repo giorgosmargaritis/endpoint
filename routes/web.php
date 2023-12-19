12ad669406a6223f7b4d57f2a62d79bb7f67e36d
Giorgos Margaritis <gmargaritis17@gmail.com>
2023-12-19 13:55:06 +0200

    set nova as main url

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (Request $request) {
-    return redirect(env('APP_URL'). '/adminpanel');
-});
+// Route::get('/', function (Request $request) {
+//     return redirect(env('APP_URL'). '/adminpanel');
+// });
