0d0cf7721971f76948f17c2e9b424c5c2c0c80e2
Giorgos Margaritis <gmargaritis17@gmail.com>
2023-12-21 10:37:01 +0200

    string replace gaps facebook

--- app/Connector/Helpers/Endpoint/EndpointHelperFacebook.php
+++ app/Connector/Helpers/Endpoint/EndpointHelperFacebook.php
@@ -170,6 +170,7 @@ class EndpointHelperFacebook extends AbstractEndpointHelper
         foreach($data as &$d)
         {
             $d['name'] = Str::upper($d['name']);
+            $d['name'] = Str::replace(' ', '', $d['column_id']);
         }
 
         return $data;
