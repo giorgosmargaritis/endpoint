f74f0d3a2d9a60e86e9168903329dfa162d7c2f6
Giorgos Margaritis <gmargaritis17@gmail.com>
2024-02-14 17:54:28 +0200

    builder $query

--- app/Nova/Connection.php
+++ app/Nova/Connection.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\ConnectionLog;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\BelongsTo;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Connection extends Resource
@@ -121,7 +121,10 @@ class Connection extends Resource
             (new \App\Nova\Actions\SendLogBatch)
             ->onlyOnDetail()
             ->canSee(function ($request) {
-                $connectionLog = $this->model()->connectionslogs->where('status', ConnectionLog::STATUS_FAIL)->orWhere('status', ConnectionLog::STATUS_PENDING)->get();
+                $connectionLog = $this->model()->connectionslogs->where(function (Builder $query) {
+                    return $query->where('status', ConnectionLog::STATUS_FAIL)
+                                 ->orWhere('status', ConnectionLog::STATUS_PENDING);
+                })->get();
                 
                 if($connectionLog)
                 {
