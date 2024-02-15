f793daf90137d34fffb16e4be9db9477b0368779
Giorgos Margaritis <gmargaritis17@gmail.com>
2024-02-15 14:42:50 +0200

    sendlog headers manipulation

--- app/Connector/Helpers/ReceiverHelper.php
+++ app/Connector/Helpers/ReceiverHelper.php
@@ -22,17 +22,20 @@ class ReceiverHelper
 
         switch ($receiver->authenticationmethod->type) {
             case AuthenticationMethod::TYPE_NOAUTH:
+                $response = Http::post($receiver->url, $transformedData);
                 break;
             
             case AuthenticationMethod::TYPE_HEADER:
                 $httpHeaders = $receiver->auth_data;
+                $response = Http::withHeaders($httpHeaders)->post($receiver->url, $transformedData);
                 break;
 
             default:
+                $response = Http::post($receiver->url, $transformedData);
                 break;
         }
         
-        $response = Http::withHeaders($httpHeaders)->post($receiver->url, $transformedData);
+        
 
         $connectionLogAttempt = ConnectionLogAttempt::create([
             'connections_logs_id' => $connectionLog->id,
